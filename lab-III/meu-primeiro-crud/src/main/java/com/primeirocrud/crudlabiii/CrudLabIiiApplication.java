824059c112ddc5c9ac3acdc178e28d002102e394
gilvaneamaro <gilvane.amaro.jr@gmail.com>
2024-03-01 19:16:32 -0300

    renomeando a pasta

---
+++ lab-III/meu-primeiro-crud/src/main/java/com/primeirocrud/crudlabiii/CrudLabIiiApplication.java
@@ -0,0 +1,15 @@
+package com.primeirocrud.crudlabiii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
+
+@SpringBootApplication
+public class CrudLabIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudLabIiiApplication.class, args);
+	}
+
+}
---
+++ lab-III/meu-primeiro-crud/src/main/java/com/primeirocrud/crudlabiii/RestController.java
@@ -0,0 +1,49 @@
+package com.primeirocrud.crudlabiii;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/celular")
+public class RestController {
+    private final List<Celular> listCelular = new ArrayList<>();
+
+    public void requestCelular(){
+        listCelular.addAll(List.of(
+                new Celular("iPhone","Apple", 1),
+                new Celular("Samsung","J5 pro",2),
+                new Celular("Motorola", "G10",3),
+                new Celular("Xiaomi","Pocophone f3", 4)
+        ));
+
+    }
+
+    @GetMapping
+    Iterable<Celular> getCelular() {
+        requestCelular();
+        return listCelular;
+    }
+
+
+    @PostMapping
+    Celular postCelular(@RequestBody Celular celular) {
+        listCelular.add(celular);
+        return celular;
+    }
+
+    @DeleteMapping
+    Celular deleteCelular(@RequestBody Celular celular){
+        listCelular.removeIf(cel -> Objects.equals(cel.getId(), celular.getId()));
+       return celular;
+   }
+
+   @PutMapping
+    Celular putCelular(@RequestBody Celular celular){
+        listCelular.add(celular);
+        return celular;
+   };
+}
