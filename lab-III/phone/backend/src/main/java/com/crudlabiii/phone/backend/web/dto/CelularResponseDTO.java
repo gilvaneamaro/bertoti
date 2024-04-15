ee33a0d4b61de5712c23995bf706b3e7c541ade4
gilvaneamaro <gilvane.amaro.jr@gmail.com>
2024-04-14 21:39:21 -0300

    Reorganizando diretorios

---
+++ lab-III/phone/backend/src/main/java/com/crudlabiii/phone/backend/web/dto/CelularResponseDTO.java
@@ -0,0 +1,17 @@
+package com.crudlabiii.phone.backend.web.dto;
+
+import com.crudlabiii.phone.backend.enums.Marca;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CelularResponseDTO {
+    private String modelo;
+    private Marca marca;
+    private String descricao;
+}
